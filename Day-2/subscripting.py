6ae59451a2e79f38736bb4a6569d57c2951b3894
kirungo <terrikirungo@gmail.com>
2026-01-21 22:36:46 +0300

    Learn: Different types of data types and their format

---
+++ Day-2/subscripting.py
@@ -0,0 +1,19 @@
+#Subscripting
+print("Hello"[0]) #will print the first element
+print("Hello"[-1]) #will print the last element
+
+#string
+print("123" + "456")
+
+#Interger = Whole number
+print(123 + 435)
+
+#Large Intergers
+print(123_456_678)
+
+#Float
+print(3.14159)
+
+#Boolean
+True
+False
