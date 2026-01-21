4fd160a0d5fdde27ede6dd77e96f86932d5bea55
kirungo <terrikirungo@gmail.com>
2026-01-21 22:47:10 +0300

    Using type() to check the data type of an object

---
+++ Day-2/data-type.py
@@ -0,0 +1,19 @@
+#Using type ot get data type
+print(type("Hello"[0])) #will print the first element
+print(type("Hello"[-1])) #will print the last element
+
+#string
+print(type("123" + "456"))
+
+#Interger = Whole number
+print(type(123 + 435))
+
+#Large Intergers
+print(type(123_456_678))
+
+#Float
+print(type(3.14159))
+
+#Boolean
+print(type(True))
+print(type(False))
