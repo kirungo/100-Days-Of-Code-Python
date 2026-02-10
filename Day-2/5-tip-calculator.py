532e19ec99dd828d3245f84357dec71177599731
kirungo <terrikirungo@gmail.com>
2026-02-10 06:15:58 +0300

    Change currency to Ksh

--- Day-2/5-tip-calculator.py
+++ Day-2/5-tip-calculator.py
@@ -1,5 +1,5 @@
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
+bill = float(input("What was the total bill? Ksh"))
 tip = int(input("What percentage tip would you like to give? "))
 people = int(input("How many people to split the bill? "))
 
@@ -9,9 +9,9 @@ total_bill = bill + tip_given
 split_per_person = total_bill / people
 
 print("Here is your receipt:")
-print(f"\tTotal bill: ${total_bill:.2f}")
-print(f"\tTip ({tip}%): ${tip_given:.2f}")
-print(f"\tEach person pays: ${split_per_person:.2f}")
+print(f"\tTip given ({tip}%): Ksh{tip_given:.2f}")
+print(f"\tTotal bill: Ksh{total_bill:.2f}")
+print(f"\tEach person pays: Ksh{split_per_person:.2f}")
 
 # Advanced: single-line calculation
 # split = (bill + (bill * tip) / 100) / people
