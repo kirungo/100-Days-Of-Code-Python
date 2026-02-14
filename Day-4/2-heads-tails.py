276750c48ebc61f12accfb94abfeb27dd71ca4a8
kirungo <terrikirungo@gmail.com>
2026-02-14 10:42:31 +0300

    Create: Head or tails game with 3 outcomes, win, lose, draw

---
+++ Day-4/2-heads-tails.py
@@ -0,0 +1,9 @@
+import random
+
+balance = random.randint(0, 2)  # 0, 1, or 2
+if balance == 1:
+    print(f"Heads! Program gave a {balance}, you win!")
+elif balance == 0:
+    print(f"Tails! Program gave a {balance}, you lose!")
+else:  # balance == 2
+    print(f"Draw! Program gave a {balance}")
