2b8661f928c51f77c6c8b0da6d28f334a13d7a88
kirungo <terrikirungo@gmail.com>
2026-02-14 10:45:31 +0300

    Decribe what 2-heads-tails.py does

--- Day-4/2-heads-tails.py
+++ Day-4/2-heads-tails.py
@@ -1,5 +1,6 @@
 import random
 
+# A coin flip program that randomly print "Heads" or "Tails" everytime it runs.
 balance = random.randint(0, 2)  # 0, 1, or 2
 if balance == 1:
     print(f"Heads! Program gave a {balance}, you win!")
