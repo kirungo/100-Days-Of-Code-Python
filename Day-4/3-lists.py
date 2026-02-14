abc6c3b1d18a48b1f24aa4302ba66950272251e3
kirungo <terrikirungo@gmail.com>
2026-02-14 11:38:38 +0300

    Change: an item in a list using its position

--- Day-4/3-lists.py
+++ Day-4/3-lists.py
@@ -9,3 +9,7 @@ print(fruits[0])
 
 # [-1] prints the last item on the list
 print(fruits[-1])
+
+# Supposing i want to change the fruit at position 2 in my list this is how i will do it
+fruits[1] = "Banana"
+print(fruits[1])
