a92c566fe8c4b9b1fb4e8c7c812d6b00e75baaf9
kirungo <terrikirungo@gmail.com>
2026-02-14 11:42:12 +0300

    Append: Adds an item at the end of the list

--- Day-4/3-lists.py
+++ Day-4/3-lists.py
@@ -10,6 +10,15 @@ print(fruits[0])
 # [-1] prints the last item on the list
 print(fruits[-1])
 
-# Supposing i want to change the fruit at position 2 in my list this is how i will do it
+# Change the fruit at index 1 (second position) in the list
 fruits[1] = "Banana"
 print(fruits[1])
+
+#to confirm index 1 fruit has change
+print(fruits)
+
+# Add to an item to the end of the list
+fruits.append("Grapes")
+
+#to confirm index append() function has worked fruit has change
+print(fruits)
