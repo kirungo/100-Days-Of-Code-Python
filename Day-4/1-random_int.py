809865d5f4a3e5a2d249efaf01273a99757275c0
kirungo <terrikirungo@gmail.com>
2026-02-14 10:18:34 +0300

    Add print statement to show what each line represents

--- Day-4/1-random_int.py
+++ Day-4/1-random_int.py
@@ -6,8 +6,12 @@ import my_module
 
 # Generate a random integer between 1 and 10 (inclusive)
 random_integer = random.randint(1, 10)
-print(random_integer)
+print(f"Generates a random integer between 1 and 10 (inclusive){random_integer}")
 
 # Access the my_pie variable from my_module
 pie = my_module.my_pie
-print(pie)
+print(f"Generated the number {pie} that I set my variable to in my locally created module ")
+
+# random.random() doesn't take any parameters and generates a random number between 0.0 to 0.9999999999999999
+random_number_0_to_1 = random.random()
+print(f"The random.random() generates a random number between 0.0 to 0.9999999999999999 e.g {random_number_0_to_1}")
