bc9c1340492d30bdde8a40a36246c50944122d38
kirungo <terrikirungo@gmail.com>
2026-02-14 11:34:39 +0300

    Print the first and last item in a list

--- Day-4/3-lists.py
+++ Day-4/3-lists.py
@@ -1,3 +1,11 @@
 # Here is what a list looks like in Python
 fruits = ["Cherry", "Apple", "Mango", "Pear"]
+
+# Prints the whole list
 print(fruits)
+
+# [0] prints the first item on the list
+print(fruits[0])
+
+# [-1] prints the last item on the list
+print(fruits[-1])
