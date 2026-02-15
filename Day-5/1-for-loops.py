d3e10cada9897c7d8f878d3e2ea388c548b40ae8
kirungo <terrikirungo@gmail.com>
2026-02-15 23:13:36 +0300

    Demonstrates basic for loop syntax with fruits list

---
+++ Day-5/1-for-loops.py
@@ -0,0 +1,12 @@
+# For Loops in Python
+
+# This is a list of fruits
+fruits = ["Apple", "Peach", "Pear", "Mango"]
+
+# for loop - fruit is the variable that lets us loop through our list
+# "fruit" is just a temporary variable name you choose
+# It holds one item from the list at a time as Python loops through
+# You could call it anything: item, x, f, etc.
+# The pattern is: for [variable_name] in [list_name]:
+for fruit in fruits:
+    print(fruit)
---
+++ Day-5/README.md
@@ -0,0 +1,17 @@
+# Day 5
+## For Loops, Range and Code Blocks
+<em> Notes: </em>
+Loops are things that hav eto happen over and over again
+
+```python
+# for loop: in the below item is the name give that allows to interate in the list
+for item in list of items:
+    #Do something to each item
+for [variable_name] in [list_name]:
+
+# for example
+fruits = ["Apple", "Peach", "Pear", "Mango"]
+
+for fruit in fruits:
+    print(fruit)
+```
