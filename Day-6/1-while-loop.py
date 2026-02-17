7ee51bbba3a2a87908022b46f0f40f567fe4597a
kirungo <terrikirungo@gmail.com>
2026-02-17 09:43:07 +0300

    Update while loop explainer comment

--- Day-6/1-while-loop.py
+++ Day-6/1-while-loop.py
@@ -4,7 +4,7 @@ def greeting():
 greeting()
 
 times_of_greetings = 6
-# The while loop will be executed until the times+of greeting is 0
+# The while loop will be executed until the times of greeting is less than 0
 while times_of_greetings > 0:
     greeting()
     times_of_greetings = times_of_greetings - 1
