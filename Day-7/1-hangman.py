1772dc5dc1c884785a317e00ae5cd907465b3565
kirungo <terrikirungo@gmail.com>
2026-02-20 23:35:19 +0300

    Working version

--- Day-7/1-hangman.py
+++ Day-7/1-hangman.py
@@ -7,20 +7,29 @@ print(random_fruit)
 blank_word = ["_"] * len(random_fruit)
 
 game_over = False
+lives_left = len(random_fruit)
 
 while not game_over:
 
     print("Word:", " ".join(blank_word))
     guess = input("Guess a letter: ")
 
-    for letter in range(len(random_fruit)):
-        if random_fruit[letter] == guess:
+    # Check letters
+    for letter in range(len(random_fruit)): 
+        if random_fruit[letter] == guess: 
             blank_word[letter] = guess
 
+    # If wrong guess, lose ONE life
     if guess not in random_fruit:
-        print("Wrong guess, please try again!")
+        lives_left -= 1
+        print(f"Wrong guess! You have {lives_left} lives left.")
 
-    # Check if game is finished
+    # If no lives left game over
+    if lives_left == 0:
+        game_over = True
+        print("You lost 😢 The word was:", random_fruit)
+
+    # If no blanks left win
     if "_" not in blank_word:
         game_over = True
         print("You won! 🎉 The word was:", random_fruit)
