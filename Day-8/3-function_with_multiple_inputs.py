6c58a0ee7d2d7eb1460046c1ab81ac62bebcb8f1
kirungo <terrikirungo@gmail.com>
2026-03-16 22:13:17 +0300

    Functions with more than 1 parameters

---
+++ Day-8/3-function_with_multiple_inputs.py
@@ -0,0 +1,7 @@
+# Function with multple inputs
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is the weather like in {location}?")
+
+greet_with("Annah", "Mombasa")
