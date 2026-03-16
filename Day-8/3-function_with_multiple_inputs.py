918895bf284a91b594a945a8389ac24baa53a534
kirungo <terrikirungo@gmail.com>
2026-03-16 22:28:36 +0300

    Differentitating between posional and keyword argument

--- Day-8/3-function_with_multiple_inputs.py
+++ Day-8/3-function_with_multiple_inputs.py
@@ -4,4 +4,12 @@ def greet_with(name, location):
     print(f"Hello {name}")
     print(f"What is the weather like in {location}?")
 
+
+# Posional Argument
 greet_with("Annah", "Mombasa")
+
+# Keyword Arguments
+greet_with(name = "Winnie", location = "Chapel")
+
+
+
